539e814bb782f7ae93ac1a12ebc976d6b81e3f3a
zwdnwlm <58358143+zwdnwlm@users.noreply.github.com>
2024-06-01 01:03:52 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -17,9 +17,9 @@ let addressesapi = [
 
 // 设置优选地址，不带端口号默认80，noTLS订阅生成
 let addressesnotls = [
-	'www.visa.com.sg#官方优选域名',
-	'www.wto.org:8080#官方优选域名',
-	'www.who.int:8880#官方优选域名',
+	//'www.visa.com.sg#官方优选域名',
+	//'www.wto.org:8080#官方优选域名',
+	//'www.who.int:8880#官方优选域名',
 ];
 
 // 设置优选noTLS地址api接口
